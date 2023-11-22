3efd29b9dbe3e3a6b426b747578a62c7827c69a0
UDUKUNDA <udukudndaelysee@gmail.com>
2023-11-22 00:26:16 +0000

    insertion_sort

--- sort.h
+++ sort.h
@@ -23,8 +23,19 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
-
-
-
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
